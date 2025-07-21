5671cda9d68f193b56a2e4f011cb80ae155e8ab3
scillidan <scillidan@yandex.com>
2025-07-21 16:36:55 +0000

    Create web-search.sh

---
+++ _arch/web-search.sh
@@ -0,0 +1,99 @@
+# https://github.com/miroslavvidovic/rofi-scripts/blob/master/web-search.sh
+
+#!/usr/bin/env bash
+
+# -----------------------------------------------------------------------------
+# Info:
+#   author:    Miroslav Vidovic
+#   file:      web-search.sh
+#   created:   24.02.2017.-08:59:54
+#   revision:  ---
+#   version:   1.0
+# -----------------------------------------------------------------------------
+# Requirements:
+#   rofi
+# Description:
+#   Use rofi to search the web.
+# Usage:
+#   web-search.sh
+# -----------------------------------------------------------------------------
+# Script:
+
+declare -A URLS
+
+URLS=(
+	["github.com"]="https://github.com/search?ref=opensearch&q="
+	["github_action"]="https://github.com/marketplace?type=actions&query="
+	["github_user"]="https://github.com/scillidan?tab=repositories&q="
+	["gist_user"]="https://gist.github.com/search?ref=searchresults&q="
+	["gist_user"]="https://gist.github.com/search?ref=searchresults&q=+user%3Ascillidan"
+	["ipaddress.com"]="https://www.ipaddress.com/website/"
+	["dmns.app"]="https://dmns.app/domains?q="
+	["allacronyms.com"]="https://www.allacronyms.com/aa-searchme?q="
+	["alternativeto.net"]="https://alternativeto.net/browse/search/?q="
+	["animegarden"]="https://gm-anime-garden.vercel.app/resources/1?search="
+	["ankiweb.net"]="https://ankiweb.net/shared/addons?search="
+	["apkpure.com"]="https://apkpure.com/search?q="
+	["automa.site"]="https://www.automa.site/marketplace?search="
+	["bilibili.com"]="https://search.bilibili.com/all?keyword="
+	["cheatsheets.zip"]="https://cheatsheets.zip/?q="
+	["ctan.org"]="https://www.ctan.org/search?phrase="
+	["devdocs.io"]="https://devdocs.io/#q="
+	["douban.com"]="https://www.douban.com/search?q="
+	["etymonline.com"]="https://www.etymonline.com/search?q="
+	["fdroid.org"]="https://search.f-droid.org/?q="
+	["filehippo.com"]="https://filehippo.com/search/?q="
+	["flathub.org"]="https://flathub.org/apps/search?q="
+	["flickr.com"]="https://www.flickr.com/search/?license=2%2C3%2C4%2C5%2C6%2C9&text="
+	["google.com"]="https://www.google.com/search?q="
+	["github.com"]="https://github.com/search?type=repositories&q="
+	["hn.algolia.com"]="https://hn.algolia.com/?dateRange=all&page=0&prefix=false&sort=byPopularity&type=story&query="
+	["hub.docker.com"]="https://hub.docker.com/search?q="
+	["imdb.com"]="https://www.imdb.com/find?q="
+	["jackett"]="127.0.0.1:9117/UI/Dashboard#filter=all&search="
+	["javascripting.com"]="https://www.javascripting.com/search?q="
+	["jsdelivr.com"]="https://www.jsdelivr.com/?query="
+	["lib.rs"]="https://lib.rs/search?q="
+	["marketplace.visualstudio.com"]="https://marketplace.visualstudio.com/search?target=VSCode&category=All%20categories&sortBy=Relevance&term="
+	["nyaa.si"]="https://nyaa.si/?f=0&c=3_0&q="
+	["ollama.com"]="https://ollama.com/library?q="
+	["open-vsx.org"]="https://open-vsx.org/?sortBy=relevance&sortOrder=desc&search="
+	["opensubtitles.org"]="https://www.opensubtitles.org/en/search2/sublanguageid-chi,eng/moviename-"
+	["scira.ai"]="https://scira.ai/?query="
+	["subhd.tv"]="https://subhd.tv/search/"
+	["yomitan"]="chrome-extension://likgccmbimhjbgkjambclfkhldnlhbnn/search.html?query="
+	["youtube.com"]="https://www.youtube.com/results?search_query="
+	["emojikeyboard.io"]="https://emojikeyboard.io/search?query="
+	["linkding"]="http://127.0.0.1:8060/bookmarks?q="
+	["msys2.org"]="https://packages.msys2.org/search?q="
+)
+
+# List for rofi
+gen_list() {
+	for i in "${!URLS[@]}"; do
+		echo "$i"
+	done
+}
+
+main() {
+	# Pass the list to rofi
+	platform=$( (gen_list) | rofi -dmenu -matching fuzzy -no-custom -location 0 -p "Search > ")
+
+	if [[ -n "$platform" ]]; then
+		query=$( (echo) | rofi -dmenu -matching fuzzy -location 0 -p "Query > ")
+
+		if [[ -n "$query" ]]; then
+			url=${URLS[$platform]}$query
+			xdg-open "$url"
+		else
+			exit
+		fi
+
+	else
+		exit
+	fi
+}
+
+main
+
+exit 0
