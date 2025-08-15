3690fc13eb5b2a24ded8925eb876cd858e4b51f5
scillidan <scillidan@yandex.com>
2025-08-15 10:00:43 +0000

    Update mount.sh

--- _arch/mount.sh
+++ _arch/mount.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-/bin/mount -t ntfs-3g UUID="F88644218643DEAE" /mnt/nvme1n1p1
 /bin/mount -t cifs //ubuntu22/sda2 /mnt/sda2 -o uid=000,gid=1000,credentials=/home/scillidan/Usr/Git/Data/srv/smb_credentials,file_mode=0664,dir_mode=0775,_netdev
