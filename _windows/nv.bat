3090724a1225ecae7fa19e3569c8bb8191bd8ca0
scillidan <scillidan@yandex.com>
2025-09-30 14:48:48 +0800

    Update nv.bat

--- _windows/nv.bat
+++ _windows/nv.bat
@@ -1,3 +1,10 @@
 @echo off
 
-neovide --size=1250x720 --frame none --no-tabs --neovim-bin nvim.exe -- -u "C:\Users\User\AppData\Local\nvim\init.lua" %*
+setlocal
+
+set "XDG_CONFIG_HOME=%USERPROFILE%\Usr\Data"
+set "XDG_DATA_HOME=%USERPROFILE%\Usr\Data"
+
+neovide --size=1250x720 --frame none --no-tabs --neovim-bin nvim.exe %*
+
+endlocal
