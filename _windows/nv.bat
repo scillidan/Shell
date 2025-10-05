bba48e9a612c907873f440d50d8be89fde90bd8a
scillidan <scillidan@yandex.com>
2025-10-05 17:40:04 +0800

    Update nv.bat

--- _windows/nv.bat
+++ _windows/nv.bat
@@ -2,8 +2,8 @@
 
 setlocal
 
-set "XDG_CONFIG_HOME=%USERPROFILE%\Usr\Data"
-set "XDG_DATA_HOME=%USERPROFILE%\Usr\Data"
+set "XDG_CONFIG_HOME=%LOCALAPPDATA%"
+set "XDG_DATA_HOME=%LOCALAPPDATA%"
 
 neovide --size=1250x720 --frame none --no-tabs --neovim-bin nvim.exe %*
 
