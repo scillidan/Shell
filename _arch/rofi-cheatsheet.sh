139f3809bc6abeec6b239984ed7351d7801e7b47
scillidan <scillidan@yandex.com>
2025-12-21 23:55:05 +0800

    update

--- _arch/rofi-cheatsheet.sh
+++ _arch/rofi-cheatsheet.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Refer to https://github.com/Zeioth/rofi-shortcuts
 
-cat ~/Usr/File/file_rofi-shortcuts/cheatsheet/*.conf | sort -u | rofi -i -dmenu -p 'cheatsheet'
+cat ~/Usr/File/file_rofi-shortcuts/{cheatsheet,cheatsheet_2zh}/*.conf | sort -u | rofi -i -dmenu -p 'cheatsheet'
--- _arch/rofi-shortcut.sh
+++ _arch/rofi-shortcut.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Refer to https://github.com/Zeioth/rofi-shortcuts
 
-cat ~/Usr/File/file_rofi-shortcuts/shortcut/*.conf | sort -u | rofi -i -dmenu -p 'shortcut'
+cat ~/Usr/File/file_rofi-shortcuts/{shortcut,shortcut_arch,shortcut_dev}/*.conf | sort -u | rofi -i -dmenu -p 'shortcut'
