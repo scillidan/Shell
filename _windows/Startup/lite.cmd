1bd1b1ccd2a611a2bb0b930dc38eb48130116873
scillidan <scillidan@yandex.com>
2025-12-28 20:20:55 +0800

    Create lite.cmd

---
+++ _windows/Startup/lite.cmd
@@ -0,0 +1,7 @@
+start "" "%USERHOME%\Usr\Data\StartMenu\Scoop Apps\Ditto.lnk
+start "" "%USERHOME%\Usr\Data\StartMenu\Scoop Apps\Espanso.lnk
+start "" "%USERHOME%\Usr\Data\StartMenu\Scoop Apps\GoldenDict.lnk
+start "" "%USERHOME%\Usr\Data\StartMenu\Scoop Apps\Keypirinha.lnk
+start "" "%USERHOME%\Usr\Data\StartMenu\Scoop Apps\RectangleWin.lnk
+start "" "%USERHOME%\Usr\Data\StartMenu\Scoop Apps\resizer2.lnk
+start "" "%USERHOME%\Usr\Data\StartMenu\Scoop Apps\Virtual Desktop Manager.lnk
