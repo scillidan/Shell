287146a5883259c17cb6f8e88469e606280866b2
scillidan <scillidan@yandex.com>
2026-01-16 09:09:20 +0800

    update

--- _windows/Other/startup_lite.cmd
+++ _windows/Other/startup_lite.cmd
@@ -15,3 +15,4 @@ timeout /t 2 /nobreak >nul
 start "" pwsh -NoProfile -ExecutionPolicy Bypass -WindowStyle Hidden -File "%USERHOME%\Usr\Git\Shell\_windows\Other\uncap_swap_capslock_esc.ps1"
 timeout /t 2 /nobreak >nul
 start "" "%SCOOP%\apps\autohotkey\current\v2\AutoHotkey64.exe" "%USERHOME%\Usr\Git\Shell\_windows\autohotkey2\ime-switcher-rshift.ahk"
+start "" "%SCOOP%\apps\autohotkey1.1\current\AutoHotkeyU64.exe" "%USERHOME%\Usr\Git\Shell\_windows\autohotkey\goldendict_search_in_group.ahk"
