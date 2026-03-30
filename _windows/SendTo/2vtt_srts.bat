d8e4a2484186c42b4d2ecd63a4b350f93d3e8670
scillidan <scillidan@yandex.com>
2026-03-30 23:57:30 +0800

    update

--- _windows/SendTo/2vtt_srts.bat
+++ _windows/SendTo/2vtt_srts.bat
@@ -1,18 +1,18 @@
 @echo off
-REM Convert LRC files to SRT format
+REM Convert SRT files to VTT format
 REM Usage:
 REM 1.1 Copy/makelink script into C:\Users\User\AppData\Roaming\Microsoft\Windows\SendTo
 REM 1.2 Select files > Context Menu > SendTo > script.bat
 REM 2. Drag and drop files onto this script
-REM 3. script.cmd <lrc1> <lrc2> ...
+REM 3. script.cmd <srt1> <srt2> ...
 
 setlocal enabledelayedexpansion
 
-set "_2srt_lrc=%USERPROFILE%\Usr\Git\Shell\lib\python\_2srt_lrc.py"
+set "srt_to_vtt=%USERPROFILE%\Usr\Git\Shell\lib\python\srt_to_vtt.py"
 
 for %%I in (%*) do (
-    set "outputFile=%%~dpnI.srt"
-    python "!_2srt_lrc!" "%%~I" "!outputFile!"
+    set "outputFile=%%~dpnI.vtt"
+    python "!srt_to_vtt!" -i "%%~I" -o "!outputFile!"
 )
 
 endlocal
