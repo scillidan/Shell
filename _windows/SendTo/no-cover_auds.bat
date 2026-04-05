296b2e661f22d164fc20377cadf2bb6c78bd6a45
scillidan <scillidan@yandex.com>
2026-04-05 15:41:02 +0800

    update

--- _windows/SendTo/no-cover_auds.bat
+++ _windows/SendTo/no-cover_auds.bat
@@ -7,24 +7,27 @@ REM 1.2 Select files > Context Menu > SendTo > script.bat
 REM 2. Drag and drop files onto this script
 REM 3. script.bat <mp31> <mp32> ...
 
+:parse_args
+if "%~1"=="" goto :end_parse
+set "originalFile=%~f1"
+set "outputFile=%~dpn1.mp3"
+set "tempFile=%~dpn1_temp_%~n1.mp3"
+set "backupFile=%~dpn1.bak"
+
+echo Processing: %~nx1
+
 setlocal enabledelayedexpansion
-
-for %%I in (%*) do (
-    set "originalFile=%%~fI"
-    set "outputFile=%%~dpnI.mp3"
-    set "tempFile=%%~dpnI_temp_%%~nI.mp3"
-    set "backupFile=%%~dpnI.bak"
-
-    echo Processing: %%~nxI
-
-    ffmpeg -i "!originalFile!" -map 0:a -c:a copy -map_metadata -1 "!tempFile!"
-    if errorlevel 1 (
-        echo FFmpeg failed
-        del /f "!tempFile!" 2>nul
-    ) else (
-        move /Y "!originalFile!" "!backupFile!" >nul
-        move /Y "!tempFile!" "!outputFile!" >nul
-        del /f "!backupFile!" >nul
-        echo Done: !outputFile!
-    )
+ffmpeg -i "!originalFile!" -map 0:a -c:a copy -map_metadata 0 "!tempFile!"
+if errorlevel 1 (
+    echo FFmpeg failed
+    del /f "!tempFile!" 2>nul
+) else (
+    move /Y "!originalFile!" "!backupFile!" >nul
+    move /Y "!tempFile!" "!outputFile!" >nul
+    del /f "!backupFile!" 2>nul
+    echo Done: !outputFile!
 )
+endlocal
+shift
+goto :parse_args
+:end_parse
